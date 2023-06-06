5f3d5d7cd1daae374bcdc056d6eff8eb026ae83a
Andrey Tsygul <vorram1@gmail.com>
2023-06-06 15:07:05 +0300

    Написан index.html

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require ('sass'));
+const concat = require('gulp-concat');
+const flatten = require('gulp-flatten');
+
+gulp.task('sass', function(done) {
+  gulp.src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./dist'));
+  
+  done();
+});
+
+gulp.task('copy-svg', function(done) {
+  gulp.src('./src/**/*.svg')
+    .pipe(flatten())
+    .pipe(gulp.dest('./dist'));
+  
+  done();
+});
+
+gulp.task('copy-png', function(done) {
+  gulp.src('./src/**/*.png')
+    .pipe(flatten())
+    .pipe(gulp.dest('./dist'));
+  
+  done();
+});
+
+gulp.task('copy-images', gulp.series('copy-svg', 'copy-png'));
+
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series('sass', 'watch'));
